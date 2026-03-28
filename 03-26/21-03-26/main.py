fe6ebac2dcee4312a74127cf886207db6ebfa191
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-03-28 08:45:15 -0300

    Correção geral de código

--- 03-26/21-03-26/main.py
+++ 03-26/21-03-26/main.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('/mnt/1962bfc0-f9ca-4c35-a3e2-7f1d23b699bd/PROGRAMAS/Cursos/Python/Estudos/03-26/14-03-26')
 from manager import TaskManager
 
-class TaskRequest(BaseModel):
+class TaskCreate(BaseModel):
     title: str
     desc: Optional[str] = ""
 
@@ -31,18 +31,18 @@ def listar_tarefas_api():
     manager.task_list = manager.load_task()
     return manager.task_list
 
-@app.get("/tarefas/{id}")
-def listtask_fromid(id: str):
-    tarefas = manager.load_task()
-    tarefa_encontrada = next((task for task in tarefas if task["ID"] == id), None)
-    if not tarefa_encontrada:
+@app.get("/tarefas/{task_id}")
+def get_task_by_id(task_id: str):
+    tasks = manager.load_task()
+    task = next((t for t in tasks if t["ID"] == task_id), None)
+    if not task:
         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
-    return tarefa_encontrada
+    return task
 
 @app.post("/tarefas", status_code=201)
-async def create_task(task_data: TaskRequest):
+def create_task(task: TaskCreate):
     try:
-        manager.new_task(task_data.title, task_data.desc)
-        return {"message": "Tarefa criada!", "all_tasks": manager.task_list}
+        manager.new_task(task.title, task.desc)
+        return {"message": "Tarefa criada!"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
